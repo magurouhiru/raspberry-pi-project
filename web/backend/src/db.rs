a14f79ababf82735cabb2c1290a283ed56c1a5b0
asdflkj169 <asdflkj169@gmail.com>
2025-05-10 19:55:22 +0000

    .db がなければ作成

--- web/backend/src/db.rs
+++ web/backend/src/db.rs
@@ -1,9 +1,21 @@
 use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
+use std::fs::{self, File};
 
 pub async fn init_db() -> Result<SqlitePool, sqlx::Error> {
+    let dir_path = if cfg!(debug_assertions) {
+        // cargo watch ignore がうまく動かなかったので開発時はtarget に作成
+        "./target/"
+    } else {
+        "./"
+    };
+    let file_name = "sqlite.db";
+
+    fs::create_dir_all(dir_path)?;
+    File::create(format!("{dir_path}{file_name}"))?;
+
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
-        .connect("sqlite://./hello.db")
+        .connect(&format!("sqlite://{}{}", dir_path, file_name))
         .await?;
 
     sqlx::query(
